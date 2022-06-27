04aae2648ab566b55610033ee037ee884f08aa58
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-27 15:41:27 +0300

    + exaple counter

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -28,3 +28,21 @@ class Slider {
     );
   }
 }
+
+"use strict";
+
+function createCounter(initial) {
+  let i = initial;//closure
+  return function counter() {
+    i++;
+    return i;
+  };
+}
+
+const counter1 = createCounter(10);
+//debugger
+counter1()
+counter1()
+console.log(counter1())
+
+const counter2 = createCounter(100);
