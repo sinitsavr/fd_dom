74f5e7fd8751cc5ee45da82ed59c553e0271b55b
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-27 15:23:38 +0300

    exapmle slider & closure

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,10 +1,10 @@
 class Slider {
   constructor(collection = [], currentIndex = 0) {
     this._collection = collection;
-    this.currentIndex = currentIndex;//number >=0 whole
+    this.currentIndex = currentIndex;
   }
-  set currentIndex(value) {  
-    if(typeof value !== 'number' || value<0 || !Number.isInteger(value)){ 
+  set currentIndex(value) {
+    if(typeof value !== 'number' || value<0 || !Number.isInteger(value)){
       throw new Error('invalid data value')
     }
     this._currentIndex = value;
@@ -18,6 +18,13 @@ class Slider {
   get currentSlide() {
     return this._collection[this._currentIndex];
   }
-  next(){ return (this._currentIndex+1)%this._collection.length; }
-  prev(){ return (this._currentIndex-1)%this._collection.length; }
+  get next() {
+    return (this._currentIndex + 1) % this._collection.length;
+  }
+  get prev() {
+    return (
+      (this._currentIndex - 1 + this._collection.length) %
+      this._collection.length
+    );
+  }
 }
