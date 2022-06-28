6851acbb176b5ac29d1d9ac2af5f4d12a4399636
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-28 15:22:41 +0300

    Example stopPropagation & capture & once

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,23 @@
 "use strict";
 
-const imagesDB = [
-  "https://upload.wikimedia.org/wikipedia/commons/5/5e/Pembroke_Welsh_Corgi_in_Grass.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Pembroke_Welsh_Corgi_dog.jpg/767px-Pembroke_Welsh_Corgi_dog.jpg",
-  "https://thumbs.dreamstime.com/b/%D0%B2%D0%B5%D0%BB%D1%88-%D0%BA%D0%BE%D1%80%D0%B3%D0%B8-%D0%BA%D0%B0%D1%80%D0%B4%D0%B8%D0%B3%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9-%D1%81%D0%BE%D0%B1%D0%B0%D1%87%D0%B8%D0%B9-%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82-%D0%B8%D0%B7%D0%BE%D0%BB%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B9-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC-159585672.jpg",
-];
+const test = document.getElementById('test')
 
-const [prevBtn, nextBtn] = document.querySelectorAll(
-  ".slider-container button"
-);
-const img = document.querySelector(".slider-container .slider img");
 
-const slider = new Slider(imagesDB);
-
-const createSlideBtnHandler =
-  (direction = "next") =>
-  () => {
-    slider.currentIndex = slider[direction === "next" ? "next" : "prev"];
-    updateView();
-  };
-
-nextBtn.addEventListener("click", createSlideBtnHandler("next"));
-prevBtn.addEventListener("click", createSlideBtnHandler("prev"));
-
-function updateView() {
-  img.setAttribute("src", slider.currentSlide);
+const btnHandler = (e) =>{
+  //e.cancelBubble = true;
+  e.stopPropagation();
+  console.group()
+  console.log(e.target)//на ком произошло событие
+  console.log(e.currentTarget)//чей обработчик отработал
+  console.groupEnd();
 }
-updateView();
+test.addEventListener('click', ()=>{console.log(1111)}, {capture:true, once:true});
+test.addEventListener('click', btnHandler, {capture:true});
+window.addEventListener('click', btnHandler);
+document.addEventListener('click', btnHandler, {capture:true});
+document.body.addEventListener('click', btnHandler);
+
+
+
+
+
--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,48 +1,14 @@
-class Slider {
-  constructor(collection = [], currentIndex = 0) {
-    this._collection = collection;
-    this.currentIndex = currentIndex;
-  }
-  set currentIndex(value) {
-    if(typeof value !== 'number' || value<0 || !Number.isInteger(value)){
-      throw new Error('invalid data value')
-    }
-    this._currentIndex = value;
-  }
-  get currentIndex() {
-    return this._currentIndex;
-  }
-  get collection() {
-    return this._collection;
-  }
-  get currentSlide() {
-    return this._collection[this._currentIndex];
-  }
-  get next() {
-    return (this._currentIndex + 1) % this._collection.length;
-  }
-  get prev() {
-    return (
-      (this._currentIndex - 1 + this._collection.length) %
-      this._collection.length
-    );
-  }
-}
-
 "use strict";
-
-function createCounter(initial) {
-  let i = initial;//closure
-  return function counter() {
-    i++;
-    return i;
-  };
-}
-
-const counter1 = createCounter(10);
+/*
+написать функцию  createAdder(n)
+которая возвращает другую функцию
+возвращенная функция должна принимать 
+аргумент m  
+и прибавлять его к n
+*/
+const createAdder = (n) => (m) => (n += m);
 //debugger
-counter1()
-counter1()
-console.log(counter1())
-
-const counter2 = createCounter(100);
+const adder = createAdder(10);
+console.log(adder(4));
+console.log(adder(4));
+console.log(adder(4));
