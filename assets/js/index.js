d69875a51809c79a297a9904e048babd41ea9704
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-28 16:36:23 +0300

    example target and event in element in HTML

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,15 @@
 "use strict";
 
-const test = document.getElementById('test')
+const btns= document.querySelectorAll('#root>button')
+const root = document.getElementById('root');
 
-
-const btnHandler = (e) =>{
-  //e.cancelBubble = true;
-  e.stopPropagation();
-  console.group()
-  console.log(e.target)//на ком произошло событие
-  console.log(e.currentTarget)//чей обработчик отработал
-  console.groupEnd();
+const btnColorHandler = ({target})=>{
+root.style.backgroundColor = target.dataset.color;
 }
-test.addEventListener('click', ()=>{console.log(1111)}, {capture:true, once:true});
-test.addEventListener('click', btnHandler, {capture:true});
-window.addEventListener('click', btnHandler);
-document.addEventListener('click', btnHandler, {capture:true});
-document.body.addEventListener('click', btnHandler);
 
-
+for (const btn of btns) {
+  btn.addEventListener ('click', btnColorHandler)
+}
 
 
 
