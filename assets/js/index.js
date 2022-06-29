d8d7b4eaec433da8b8f8f1a13b5c24ee9b0fb9c5
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-29 16:27:04 +0300

    Refactor createActorsCard v2.0 Add stringToColour

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,60 @@
 "use strict";
 
-const btns= document.querySelectorAll('#root>button')
-const root = document.getElementById('root');
+const cardsContainer = document.getElementById('cards-container');
 
-const btnColorHandler = ({target})=>{
-root.style.backgroundColor = target.dataset.color;
+const HTMLCollectionActors = actors.map((actor)=>createActorsCard(actor));
+
+function createActorsCard(actor){
+  const HTMLCard = document.createElement('li');
+  HTMLCard.classList.add('card-wrapper');
+
+  const cardContainer = document.createElement('article');
+  cardContainer.classList.add('card-container');
+
+  const cardPhotoWrapper = document.createElement('div');
+  cardPhotoWrapper.classList.add('card-photo-wrapper');
+
+  const cardInitials = document.createElement('div');
+  cardInitials.classList.add('card-initials');
+  cardInitials.style.backgroundColor = stringToColour(actor.name);
+  cardInitials.append(document.createTextNode(actor.name[0]));//HW
+
+  const cardPhoto = document.createElement('img');
+  cardPhoto.classList.add('card-photo');
+  cardPhoto.setAttribute('src', actor.photo);
+  cardPhoto.setAttribute('alt', actor.name);
+  cardPhoto.addEventListener('error', handlePhotoError)
+
+  cardPhotoWrapper.append(cardInitials, cardPhoto)
+
+  const cardName = document.createElement('h2');
+  cardName.classList.add('card-name');
+  cardName.append(document.createTextNode(actor.name));
+
+  const cardInfo = document.createElement('p');
+  cardInfo.classList.add('card-info');
+  cardInfo.append(document.createTextNode(actor.birthdate));
+
+  cardContainer.append(cardPhotoWrapper, cardName, cardInfo);
+  HTMLCard.append(cardContainer);
+  return HTMLCard;
+} 
+
+cardsContainer.append(...HTMLCollectionActors);
+
+function handlePhotoError({target}){
+  target.remove();
 }
 
-for (const btn of btns) {
-  btn.addEventListener ('click', btnColorHandler)
+function stringToColour(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).slice(-2);
+  }
+  return colour;
 }
-
-
-
