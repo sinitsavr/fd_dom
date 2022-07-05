f9ad081e502dcb793e0d36f44e6deb3aa227ae64
Vadim Sinitsa <sinitsavr@gmail.com>
2022-07-05 14:39:51 +0300

    Example JSON serialize/deserialize

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,60 +1,25 @@
 "use strict";
 
-const cardsContainer = document.getElementById('cards-container');
-
-const HTMLCollectionActors = actors.map((actor)=>createActorsCard(actor));
-
-function createActorsCard(actor){
-  const HTMLCard = document.createElement('li');
-  HTMLCard.classList.add('card-wrapper');
-
-  const cardContainer = document.createElement('article');
-  cardContainer.classList.add('card-container');
-
-  const cardPhotoWrapper = document.createElement('div');
-  cardPhotoWrapper.classList.add('card-photo-wrapper');
-
-  const cardInitials = document.createElement('div');
-  cardInitials.classList.add('card-initials');
-  cardInitials.style.backgroundColor = stringToColour(actor.name);
-  cardInitials.append(document.createTextNode(actor.name[0]));//HW
-
-  const cardPhoto = document.createElement('img');
-  cardPhoto.classList.add('card-photo');
-  cardPhoto.setAttribute('src', actor.photo);
-  cardPhoto.setAttribute('alt', actor.name);
-  cardPhoto.addEventListener('error', handlePhotoError)
-
-  cardPhotoWrapper.append(cardInitials, cardPhoto)
-
-  const cardName = document.createElement('h2');
-  cardName.classList.add('card-name');
-  cardName.append(document.createTextNode(actor.name));
-
-  const cardInfo = document.createElement('p');
-  cardInfo.classList.add('card-info');
-  cardInfo.append(document.createTextNode(actor.birthdate));
-
-  cardContainer.append(cardPhotoWrapper, cardName, cardInfo);
-  HTMLCard.append(cardContainer);
-  return HTMLCard;
-} 
-
-cardsContainer.append(...HTMLCollectionActors);
-
-function handlePhotoError({target}){
-  target.remove();
+const user = {
+  name:"Elon",
+  age:51,
+  getName(){return this._name},
+  isWife: true,
+  children:['ch1','ch2'],
+  addres:{
+    country:'USA',
+    town:{
+      name:'NY',
+      ok:123
+    },
+  },
+  ukrPasport: null,
+  petty: undefined,
+  [Symbol('prop symbol')]: 'prop symbol',
 }
+console.log(user)
+const serializeUser = JSON.stringify(user);
+console.log(serializeUser)
 
-function stringToColour(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  let colour = '#';
-  for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xFF;
-    colour += ('00' + value.toString(16)).slice(-2);
-  }
-  return colour;
-}
+const deserializeUser = JSON.parse(serializeUser)
+console.log(deserializeUser)
--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,14 +1,16 @@
 "use strict";
-/*
-написать функцию  createAdder(n)
-которая возвращает другую функцию
-возвращенная функция должна принимать 
-аргумент m  
-и прибавлять его к n
-*/
-const createAdder = (n) => (m) => (n += m);
-//debugger
-const adder = createAdder(10);
-console.log(adder(4));
-console.log(adder(4));
-console.log(adder(4));
+
+// const pattern1 = new RegExp('\\b[A-Z][a-z]{2,12}\\b','gi');
+// const pattern2 = /[0-9]{5,12}\b/g;
+
+const pattern2 =/\b(\d{4})([a-z]+)\b/g;
+const str = '788.All 1234ddd my trouble 1956ddd seemed so 432 far away';
+//'788.All 1234year my trouble 1956year seemed so 432 far away';
+// if(pattern2.test(str)){
+//   console.log(pattern2)
+// }else{
+//   console.log(str)
+// }
+
+const newStr = str.replace(pattern2, '$2: $1')
+console.log(newStr)
