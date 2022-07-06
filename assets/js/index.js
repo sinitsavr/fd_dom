1720671ca567558437e3688858899c3417030d4f
Vadim Sinitsa <sinitsavr@gmail.com>
2022-07-06 16:20:18 +0300

    Example deligation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,20 @@
 "use strict";
 
-function handlePromise(promise){
-  return promise.then((data)=>{
-    console.log('Promise: ', data)
-  })
-}
+/* const list = document.getElementById('article');
+list.addEventListener('click', (event)=>{
+  console.log(event.target)
+  event.target.innerText += '!!!'
+}) */
 
-const number = 777;
-
-handlePromise(Promise.resolve(number))
+const article = document.getElementById('article');
+article.addEventListener('click', ({target})=>{
+  if (target === article) {
+    return;
+  }
+  for (const child of article.children){
+    child.style.fontStyle = '';
+    child.style.fontWeight = '';
+  }
+target.style.fontStyle = 'italic';
+target.style.fontWeight = 700;
+})
