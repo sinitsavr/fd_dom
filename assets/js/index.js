87849bf7747358707315c7b5ed23e384edb5f2f0
Vadim Sinitsa <sinitsavr@gmail.com>
2022-07-06 15:46:40 +0300

    Example Promise.resolve(number)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,11 @@
 "use strict";
 
-function loadImage(pathSrc) {
-  const image = new Image(); //<img >
-  image.src = pathSrc; //load||error //<img src='pathSrc'>
-  return new Promise((resolve, reject) => {
-    image.addEventListener("load", 
-    () => {
-      resolve(image);
-    });
-    image.addEventListener("error", 
-    () => {
-      reject(new Error("path invalid"));
-    });
-  });
+function handlePromise(promise){
+  return promise.then((data)=>{
+    console.log('Promise: ', data)
+  })
 }
 
-loadImage("https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg")
-  .then((data)=>{
-    document.body.append(data)
-  })
-  .catch((error)=>{
-    document.body.append(error)
-  })
+const number = 777;
+
+handlePromise(Promise.resolve(number))
